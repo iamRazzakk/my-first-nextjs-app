62588b2d338e04b6dfd89e3adc7fcb1cac4483d1
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-12-05 17:07:54 +0600

    knowing about grouping route

---
+++ src/app/(withLayout)/About/Contact/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Tis is Contact page for group route.</h1>
+        </div>
+    );
+};
+
+export default ContactPage;
