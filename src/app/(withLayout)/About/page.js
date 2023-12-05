686dfb654290f53d8941d344a0fcee3ed240769b
Md Abdur Razzak ~ <rakibt23p@gmail.com>
2023-12-05 17:53:24 +0600

    git commit

--- src/app/(withLayout)/About/page.js
+++ src/app/(withLayout)/About/page.js
@@ -1,5 +1,8 @@
 import React from 'react';
-
+export const metadata = {
+    title: "About page",
+    description: "This is about page",
+  };
 const AboutPage = () => {
     return (
         <div>
